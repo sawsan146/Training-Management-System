4d85a9eca116c3d408ad4a9a9056acd6c249c62f
Sawsan Elsebaey <sawsanelsebay2@gmail.com>
2025-08-18 01:46:13 +0300

    #IGenericRepository , GenericRepository

---
+++ Repository/Interface/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingSys.Core.Interface
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] includes);
+
+        Task<T> GetByIdAsync(int id);
+        Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes);
+
+        Task AddAsync(T entity);
+
+        Task DeleteByIdAsync(int id);
+
+        Task UpdateAsync(T entity);
+
+    }
+}
---
+++ Training_Sys.Infrastructure/Repository/GenericRepository.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Training_Sys.Infrastructure.Data;
+using TrainingSys.Core.Interface;
+
+namespace Training_Sys.Infrastructure.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _context.Set<T>().AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var entity= await _context.Set<T>().FindAsync(id);
+            _context.Set<T>().Remove(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync() 
+            => await _context.Set<T>().AsNoTracking().ToListAsync();
+
+     
+        public async Task<IReadOnlyList<T>> GetAllAsync(params Expression<Func<T, object>>[] includes)
+        {
+
+            var query = _context.Set<T>().AsQueryable();
+
+            foreach (var include in includes)
+            {
+                query.Include(include);
+            }
+            return query.ToList();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            var entity=await _context.Set<T>().FindAsync(id);
+            return entity;
+        }
+
+        public async Task<T> GetByIdAsync(int id, params Expression<Func<T, object>>[] includes)
+        {
+            var query = _context.Set<T>().AsQueryable();
+
+            foreach (var include in includes)
+            {
+                query.Include(include);
+            }
+
+            var entity= await query.FirstOrDefaultAsync(x=>EF.Property<int>(x,"Id")==id);
+
+            return entity;
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
